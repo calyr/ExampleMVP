ede09a0441f46dd11ec4aa55b3ac6dca615f9283
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2020-03-05 22:35:48 -0400

    Implementación en el metodo login del Presentador para validar que el campo ingresado es correcto (Solo para fines de validación)

--- app/src/main/java/com/calyrsoft/examplemvp/LoginPresenter.kt
+++ app/src/main/java/com/calyrsoft/examplemvp/LoginPresenter.kt
@@ -1,14 +1,18 @@
 package com.calyrsoft.examplemvp
 
 import android.content.Context
-import android.content.res.Resources
 
 class LoginPresenter(val view: ILoginView, val context: Context): ILoginPresenter {
     override fun login(userName: String, password: String) {
         if( userName.isEmpty()) {
             view.showErrorUserName( context.getString(R.string.login_form_username_empty))
         } else {
-            view.showMessage(context.getString(R.string.login_form_success))
+            if( userName == "calyr") {
+                view.showMessage(context.getString(R.string.login_form_success))
+            } else {
+                view.showMessage(context.getString(R.string.login_form_error))
+            }
+
         }
     }
 }
