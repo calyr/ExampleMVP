e3b57047fd77d5f43fdd07d365e0b2873d386744
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2020-03-05 22:14:07 -0400

    Adicion de la vista(ILoginView) en el constructor en la clase LoginPresenter

--- app/src/main/java/com/calyrsoft/examplemvp/LoginPresenter.kt
+++ app/src/main/java/com/calyrsoft/examplemvp/LoginPresenter.kt
@@ -1,6 +1,7 @@
 package com.calyrsoft.examplemvp
 
-class LoginPresenter: ILoginPresenter {
+class LoginPresenter(val view: ILoginView): ILoginPresenter {
     override fun login(userName: String, password: String) {
+
     }
 }
