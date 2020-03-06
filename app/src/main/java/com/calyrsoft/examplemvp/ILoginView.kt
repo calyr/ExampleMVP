3f2576b044c5b5f0065db84730d1d757e4c6c7a7
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2020-03-05 21:51:19 -0400

    Adición de metodos en ILoginView

--- app/src/main/java/com/calyrsoft/examplemvp/ILoginView.kt
+++ app/src/main/java/com/calyrsoft/examplemvp/ILoginView.kt
@@ -1,4 +1,7 @@
 package com.calyrsoft.examplemvp
 
 interface ILoginView {
+    fun showErrorUserName(message: String)
+    fun showErrorPassword(message: String)
+    fun showMessage(message: String)
 }
