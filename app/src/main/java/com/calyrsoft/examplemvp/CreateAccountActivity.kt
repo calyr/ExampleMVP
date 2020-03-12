6ee41f20f9480c6298a38529986b9c3a9611731f
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2020-03-11 22:38:36 -0400

    Subreescritura de metodos para mostrar el item en el toolbar (onCreateOptionsMenu, onOptionItemSelected)

--- app/src/main/java/com/calyrsoft/examplemvp/CreateAccountActivity.kt
+++ app/src/main/java/com/calyrsoft/examplemvp/CreateAccountActivity.kt
@@ -2,6 +2,9 @@ package com.calyrsoft.examplemvp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_create_account.*
 
 class CreateAccountActivity : AppCompatActivity() {
@@ -15,5 +18,22 @@ class CreateAccountActivity : AppCompatActivity() {
             it?.setDisplayShowHomeEnabled(true)
             title = getString(R.string.login_activity_name)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_create_account, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       return when (item.itemId) {
+            R.id.setting_menu_item -> {
+                Toast.makeText(this, R.string.app_name, Toast.LENGTH_LONG).show()
+                true
+            }
+           else -> {
+                true
+           }
+       }
     }
 }
